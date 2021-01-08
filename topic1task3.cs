1fc058885db94d1da8dd2e41e38e2f3083f4f4a4
SergeyMurzak <murzaksergey@gmail.com>
2021-01-08 22:42:58 +0300

    third commit

---
+++ topic1task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tast1_2_iJunior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inkriment = 0;
+
+            Console.WriteLine(inkriment++);
+            Console.WriteLine(++inkriment);
+            Console.WriteLine(inkriment++);
+            Console.WriteLine(++inkriment);
+            Console.ReadKey();
+        }
+    }
+}
