9c01114b4a0a8c12e0de5d4735fdf37dd5b9c40e
SergeyMurzak <murzaksergey@gmail.com>
2021-01-09 00:34:56 +0300

    task 4 done

---
+++ iJuniorTask4/iJuniorTask4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iJuniorTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfGold;
+            int amountOfCristals;
+            int costOfCristal = 47;
+            float youGet;
+            int change;
+           
+
+            Console.Write("Сколько вам нужно кристалов? :");
+            amountOfCristals = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Достаю из кармана:");
+            amountOfGold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вот все, что есть!");
+            youGet = amountOfGold/(amountOfCristals * costOfCristal );
+            change = amountOfGold%(amountOfCristals * costOfCristal );
+            Console.Write("Получи свои " + youGet + " остальные " + change + " оставь себе");
+
+            
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
