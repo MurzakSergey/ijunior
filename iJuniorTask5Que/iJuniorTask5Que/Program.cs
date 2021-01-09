25001bb6bd0c332a3266757ec2f43e6293944b87
SergeyMurzak <murzaksergey@gmail.com>
2021-01-09 23:42:13 +0300

    task 5 done

---
+++ iJuniorTask5Que/iJuniorTask5Que/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iJuniorTask5Que
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waitingPerPerson = 10; //минут
+            int amountOfPeople;
+            float waitingTimeHours, waitingTimeMin;
+            Console.Write("Сколько перед вами старушек?: ");
+            amountOfPeople = Convert.ToInt32(Console.ReadLine());
+
+            waitingTimeHours = amountOfPeople * waitingPerPerson/60;
+            waitingTimeMin = Convert.ToSingle(amountOfPeople) * waitingPerPerson % 60;
+            Console.WriteLine("Вам ждать: " + waitingTimeHours + " часа "+ waitingTimeMin + " минут. Подумайте о смысле жизни");
+            Console.ReadKey();
+        }
+    }
+}
