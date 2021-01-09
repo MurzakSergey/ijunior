66e104c7d8b06714f788f0f0ffe9d14a504680a9
SergeyMurzak <murzaksergey@gmail.com>
2021-01-10 00:48:25 +0300

    task6Done

---
+++ iJuniorTask4/additionalTask6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace additionalTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, sign;
+            int age;
+            Console.Write("Введите имя?:");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет?:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кто вы по зодиаку?:");
+            sign = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {name}, вам {age} лет, вы {sign}");
+            Console.ReadKey();
+            
+           
+        }
+    }
+}
