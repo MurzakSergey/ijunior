c4becc31729a8dc7060b157e3cd2576046c0a760
SergeyMurzak <murzaksergey@gmail.com>
2021-01-12 23:29:50 +0300

    added practice from task 2

---
+++ iJunior_topic_2/iJunior_topic_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iJunior_topic_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Практика - задание первое
+            //string password = "qwerty";
+            //string answer;
+
+            //Console.Write("Пароль! (угрожающе смотрю):");
+            //answer = Console.ReadLine();
+
+
+            //if (password == answer)
+            //{
+            //    Console.WriteLine("Угадал, ага, молодец");
+
+            //}
+            //else
+            //{ Console.WriteLine("У- Уходи!");
+            //}
+
+
+            // Практика - задание второе
+
+            double bitcoinUSD = 35000;
+            double amountOfBitcoin;
+            double usdBitcoin = 1/bitcoinUSD;
+            double amountOfUsd;
+            double answer;
+
+           
+            Console.WriteLine("Что хотите купить?:" +
+                " Доллары - нажмите 1, " +
+                " Биткоин - нажмите 2");
+
+            answer = Convert.ToDouble(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 1:
+
+
+                    Console.WriteLine("Сколько нужно долларов?:");
+
+                    amountOfUsd = Convert.ToDouble(Console.ReadLine());
+                    if (amountOfUsd != 1337)
+                    {    
+                        Console.WriteLine("С вас: " + amountOfUsd * usdBitcoin + " биткоинов");
+                    }
+
+                    else
+                    { Console.WriteLine("Поздравляем! Вы выиграли ДЖЕКПОТ!"); }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Сколько нужно биткоинов?:");
+                    amountOfBitcoin = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("С вас: " + amountOfBitcoin * bitcoinUSD + " долларов");
+
+                    break;
+
+
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
