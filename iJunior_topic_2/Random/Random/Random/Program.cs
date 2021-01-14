a0cceac7e658460ec384530c588f9f61db1c9c9e
SergeyMurzak <murzaksergey@gmail.com>
2021-01-15 00:13:29 +0300

    impruved WhatIsTheNumber

--- iJunior_topic_2/Random/Random/Random/Program.cs
+++ iJunior_topic_2/Random/Random/Random/Program.cs
@@ -11,19 +11,22 @@ namespace WhatIsTheNumber
         static void Main(string[] args)
         {
             int larger, lower, number, answer;
-            int attempts = 10;
+            int attempts = 2;
             lower = 1;
-            larger = 6;
+            larger = 100;
             Random rand = new Random();
-            
-
-            for (int i=0; i<attempts; i++)
-               
-
-            {
-                number = rand.Next(lower, larger);
-                Console.WriteLine("Угадайте число от " + lower + " до " + (larger-1) + " : ");
+            number = rand.Next(lower, larger);
+            Console.WriteLine("Угадайте число от " + lower + " до " + (larger - 1) + " с " + attempts + " попыток: ");
+            for (int i=1; i<=attempts; i++)
+            {                            
                 answer = Convert.ToInt32(Console.ReadLine());
+
+                if (i == attempts && answer != number)
+                {
+                    Console.WriteLine("Может повезет в следующий раз! Это было число " + number + "!");
+                    break;    
+                }
+
 
                 if (answer == number)
                 {
@@ -33,12 +36,15 @@ namespace WhatIsTheNumber
 
                 else
                 {
-                    Console.WriteLine("Давай еще раз! У тебя " + (attempts - i-1) + " попыток" + number);
+                    Console.WriteLine("У тебя " + (attempts - i) + " попыток" + "                       подсказка, только тихо! это" + number);
                                     
                 }
 
-              
-            }
+                
+
+                }
+
+            
 
             Console.ReadKey();
 
