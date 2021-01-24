ed65182d11b386e936963ff26334060274a00d07
SergeyMurzak <murzaksergey@gmail.com>
2021-01-24 22:27:30 +0300

    've finished Truffles

--- iJunior_topic_2/TheGame/Truffles/Truffles/Program.cs
+++ iJunior_topic_2/TheGame/Truffles/Truffles/Program.cs
@@ -18,26 +18,30 @@ namespace Truffles
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
-            {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
-            {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
+            {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','$',' ',' ',' ',' ',' ',' ',' ','|'},
+            {'|',' ',' ','$',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ','I','I','I','I',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ','I',' ',' ','I',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ','I',' ',' ','I',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ','I',' ',' ','I',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
-            {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
+            {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','$',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
             {'T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T','T'}
             };
+                     
+
+
 
             //int[,] delta = new int[10, 10];
             int posX = 1, posY = 1;
+            char[] bag = new char[1];
             while (playing)
-            { 
-
+            {
+                Console.WriteLine("You are pig. Pick up truffles as $ using arrows! ");
             for (int i=0; i<map.GetLength(0); i++)
             {
                 for (int j=0; j<map.GetLength(1); j++)
@@ -46,39 +50,61 @@ namespace Truffles
                 }
                Console.Write("\n");
             }
-
-
+                
+                
+                Console.SetCursorPosition(0, 20);
+                Console.Write("Score: ");
+                for (int bagLenth = 0; bagLenth < bag.Length; bagLenth++)
+                {
+                    bag[bagLenth] = 'X';
+                    Console.Write(bag[bagLenth]);
+                }
+                
                 // Console.SetCursorPosition (delta[1,0]+posY, delta[0,1]+posX);
-                Console.SetCursorPosition(posX, posY);
+                Console.SetCursorPosition(posX, posY+1);
                 Console.WriteLine('C');
                 ConsoleKeyInfo move = Console.ReadKey();
 
                 switch (move.Key)
                 {
                     case ConsoleKey.DownArrow:
-                        if (map[posY+1, posX] == ' ')
+                        if (map[posY+1, posX] == ' ' || map[posY + 1, posX] == '$')
                         { posY++; }
                         break;
 
                     case ConsoleKey.UpArrow:
-                        if (map[posY-1, posX] == ' ')
+                        if (map[posY-1, posX] == ' ' || map[posY - 1, posX] == '$')
                         { posY--; }
                         break;
 
                     case ConsoleKey.LeftArrow:
-                        if (map[posY, posX-1] == ' ')
+                        if (map[posY, posX-1] == ' ' || map[posY, posX - 1] == '$')
                         { posX--; }
                         break;
 
                     case ConsoleKey.RightArrow:
-                        if (map[posY, posX+1] == ' ')
+                        if (map[posY, posX+1] == ' ' || map[posY, posX + 1] == '$')
                         { posX++; }
                         break;
                 }
-                
+
+                if (map[posY, posX] == '$')
+                {
+                    map[posY, posX] = 'o';
+                    char[] tempBag = new char[bag.Length + 1];
+                    for (int tbLenth = 0; tbLenth < bag.Length; tbLenth++)
+                    {
+                        tempBag[tbLenth] = bag[tbLenth];
+                    }
+                    tempBag[tempBag.Length - 1] = 'X';
+                    bag = tempBag;
+                }
+
                 Console.Clear();
-               
-                
+
+
+
+
 
             }
 
