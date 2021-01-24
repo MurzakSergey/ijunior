87f4c6c7a3ec0f52c027581efb9d976ca378cea1
SergeyMurzak <murzaksergey@gmail.com>
2021-01-25 00:06:05 +0300

    add part of arrays homework

---
+++ iJunior_topic_2/homework/homework/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Random rand = new Random();
+                    
+
+
+            int[,] matrix = new int[10, 10];
+
+            for (int i=0; i<matrix.GetLength(0); i++ )
+            {
+                for (int j = 0; j< matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rand.Next(0, 100);
+
+                    Console.Write(matrix[i, j ] + " ");
+                           
+                              
+
+
+                }
+                Console.WriteLine("\n");
+            }
+
+            foreach (int cell in matrix)
+            {
+
+                if (cell >= number)
+                {
+                number = cell;
+                }
+
+            }
+
+            Console.WriteLine(number);
+
+           
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                       if (number == matrix[i, j])
+                    {
+                        matrix[i, j] = 0;
+                    }
+
+                        Console.Write(matrix[i, j] + " ");
+
+
+
+
+                    }
+                    Console.WriteLine("\n");
+                }
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
