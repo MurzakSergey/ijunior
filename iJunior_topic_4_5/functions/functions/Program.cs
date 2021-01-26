fed478d83c451a1d0215a83ef81d4533710e651e
SergeyMurzak <murzaksergey@gmail.com>
2021-01-26 23:36:25 +0300

    started new topic - functions

---
+++ iJunior_topic_4_5/functions/functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool dummy = true;
+            while (dummy)
+            {
+                String ask = Console.ReadLine();
+                writeError("You are dummy");
+                if (ask == "exit")
+                {
+                    dummy = false;
+                }
+
+
+            }
+        }
+
+        static void writeError(string text)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = defaultColor;
+         }
+
+
+    }
+}
