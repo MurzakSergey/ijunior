ed48c0e08283476b2e24f8868abdbb422ca62e71
SergeyMurzak <murzaksergey@gmail.com>
2021-01-27 00:05:20 +0300

    little but important example

---
+++ iJunior_topic_4_5/functions2/functions2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 3;
+            int t = 5;
+            int rPlusT;
+            rPlusT = Sum(r, t)+r*t;
+
+
+            Console.WriteLine(rPlusT);
+            Console.WriteLine(Sum(r,t));
+            Console.ReadLine();
+        }
+
+        static int Sum(int x, int y)
+            {
+
+            //return x + y;
+            // or 
+
+            int sum;
+            sum = x + y;
+            return sum;
+            
+
+            
+            
+            }
+
+    }
+}
