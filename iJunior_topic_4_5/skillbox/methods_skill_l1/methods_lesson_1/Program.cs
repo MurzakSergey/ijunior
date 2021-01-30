cffa87dced745b69b3693900fafbba0715687f84
SergeyMurzak <murzaksergey@gmail.com>
2021-01-31 01:11:44 +0300

    skill methods l1

--- iJunior_topic_4_5/skillbox/methods_skill_l1/methods_lesson_1/Program.cs
+++ iJunior_topic_4_5/skillbox/methods_skill_l1/methods_lesson_1/Program.cs
@@ -17,12 +17,40 @@ namespace methods_lesson_1
             // Организовать заполнение и вывод на экран каждого из них
             // Найти максимальный и минимальный элементы в каждом массиве
 
-            int[] kindergarden = new int[];
-            int[] school = new int[];
+            Random r1020 = new Random();
+            int rkind = r1020.Next(5, 15);
+            int rschool = r1020.Next(5, 15);
+            int[] kindergarden = new int[rkind];
+            int[] school = new int[rschool];
 
+            Children(kindergarden, 1, 75);
 
+            Children(school, 1, 100);
+            Console.ReadLine();
+            
+         }
+        static void Children(int[] array, int min, int max)
+        {
+            Random r1020 = new Random();
+            int maxVal = min;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int rk = r1020.Next(min, max);
+                array[i] = rk;
+                ConsoleColor blue = ConsoleColor.Blue;
+                Console.ForegroundColor = blue;
+                Console.Write(i + 1 + ".");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(array[i] + " ");
+                
+                if (array[i] >= maxVal)
+                { maxVal = array[i]; }
+             }
 
-
+            Console.Write("\n"+"Max is " + maxVal +"\n\n");
         }
-    }
+         
+
+
+     }
 }
