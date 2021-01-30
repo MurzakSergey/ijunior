7b34fef3036e236af1b506c22c718e06a121d191
SergeyMurzak <murzaksergey@gmail.com>
2021-01-31 00:00:08 +0300

    skill

---
+++ iJunior_topic_4_5/skillbox/methods_skill_l1/methods_lesson_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1. Разделение логики: использование методов
+
+            // Есть два массива содержащие не менее 10 и не более 20 элементов элементов. 
+            // Организовать заполнение и вывод на экран каждого из них
+            // Найти максимальный и минимальный элементы в каждом массиве
+
+            int[] kindergarden = new int[];
+            int[] school = new int[];
+
+
+
+
+        }
+    }
+}
