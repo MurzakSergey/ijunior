53f06dd9e4f38f73aa2e1b6c0bd33937c57b811d
SergeyMurzak <murzaksergey@gmail.com>
2021-02-08 23:59:59 +0300

    c

---
+++ iJunior_topic_4_5/skillbox/methods_skill_l3/methods_skill_l3/methods_skill_l3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_skill_l3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
