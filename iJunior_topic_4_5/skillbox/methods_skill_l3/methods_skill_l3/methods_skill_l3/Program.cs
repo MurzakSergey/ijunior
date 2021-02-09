04913cbe806dd9fef58ece6b093bdb02bf08c70b
SergeyMurzak <murzaksergey@gmail.com>
2021-02-09 23:59:21 +0300

    p

--- iJunior_topic_4_5/skillbox/methods_skill_l3/methods_skill_l3/methods_skill_l3/Program.cs
+++ iJunior_topic_4_5/skillbox/methods_skill_l3/methods_skill_l3/methods_skill_l3/Program.cs
@@ -10,6 +10,13 @@ namespace methods_skill_l3
     {
         static void Main(string[] args)
         {
+        int[] bigWhite = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Method(bigWhite);
+        }
+
+        static void Method(params int[] array)
+        {
+            Console.WriteLine(array[1]);
         }
     }
 }
